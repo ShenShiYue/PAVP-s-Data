7fde87ee102c33de04e4c4984731aa6ee8e6875c
ShenShiYue <948251678@qq.com>
2024-06-02 17:07:12 +0800

    Update app.py

--- 2/app.py
+++ 2/app.py
@@ -16,12 +16,14 @@ st.set_page_config(
     initial_sidebar_state="auto"  #侧边栏
     )
 #获得序列
-text = st.text_area(label = '请输入fasta格式的氨基酸序列：（例：>p5/nNEMSWWMSHLIA）', 
+text = st.text_area(label = '请输入fasta格式的氨基酸序列：（例：>p5 NEMSWWMSHLIA）', 
                     value='请输入...', 
                     height=2, 
                     max_chars=200, 
                     help='最大长度限制为200')
 st.write('您的输入是', text)
+if st.button('确定'):
+    st.write('提交成功')
 # text='>p5/nNEMSWWMSHLIA'
 #定义函数
 def read_fasta(fname):
